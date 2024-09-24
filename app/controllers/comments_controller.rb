a8282e344709f225c4387efae733066e4661635a
markalbrand56 <mark.albrand5@gmail.com>
2024-09-24 16:46:36 -0600

    lint corrections

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,5 +10,4 @@ class CommentsController < ApplicationController
     def set_post
       @post = Post.find(params[:post_id])
     end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts do 
+  resources :posts do
     resources :comments
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
