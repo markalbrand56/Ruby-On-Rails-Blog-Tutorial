fe31dfb7946e7aee42e45ed8856bb06d9c2ce89a
markalbrand56 <mark.albrand5@gmail.com>
2024-09-25 00:15:26 -0600

    tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates_presence_of :title # Title cant be blank
   has_rich_text :content
-  has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy # Delete cascade basically
 end
