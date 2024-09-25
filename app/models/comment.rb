34ae9a8cd21a8db30d44994d9a50dd1d5d84f8e3
markalbrand56 <mark.albrand5@gmail.com>
2024-09-25 00:07:23 -0600

    live comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   broadcasts_to :post # This broadcasts the creation, to update the view
+  # this shows any update in real time in the view
 end
