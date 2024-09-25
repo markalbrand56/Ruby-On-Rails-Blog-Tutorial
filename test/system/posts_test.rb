3e26fb29632ba9959478872c0b79cd207d0477e5
markalbrand56 <mark.albrand5@gmail.com>
2024-09-25 00:27:04 -0600

    tests

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -14,7 +14,8 @@ class PostsTest < ApplicationSystemTestCase
     visit posts_url
     click_on "New post"
 
-    fill_in "Content", with: @post.content
+    # fill_in "Content", with: @post.content
+    find('trix-editor').set(@post.content)
     fill_in "Title", with: @post.title
     click_on "Create Post"
 
